2a104f72a6753a5ee0e5461ecf11f9087c15bd49
Monteiro <monteiro.lcm@gmail.com>
2019-05-14 00:36:31 +0100

    update: CMakes

--- CMakeTools.cmake
+++ CMakeTools.cmake
@@ -1,7 +1,7 @@
 # -------------------------------------------------------------------------------------------------
-# create container
+# create package
 # -------------------------------------------------------------------------------------------------
-function(create_container)
+function(create_package)
     set(options)
     set(oneValue   TARGET)
     set(multiValue SOURCES PUBLIC_INCLUDES PRIVATE_INCLUDES PUBLIC_LINKS PRIVATE_LINKS)
@@ -30,6 +30,24 @@ function(create_interface)
     set(multiValue INCLUDES LINKS)
     cmake_parse_arguments(ARG "${options}" "${oneValue}" "${multiValue}" ${ARGN} )
     # object library
+    add_library(${ARG_TARGET} INTERFACE)
+    # dependencies
+    target_include_directories(${ARG_TARGET} 
+        INTERFACE ${ARG_INCLUDES})
+    foreach(L ${ARG_LINKS})
+        target_include_directories(${ARG_TARGET} 
+            INTERFACE $<TARGET_PROPERTY:${L},INTERFACE_INCLUDE_DIRECTORIES>)
+    endforeach()
+endfunction()
+# -------------------------------------------------------------------------------------------------
+# create collector
+# -------------------------------------------------------------------------------------------------
+function(create_collector)
+    set(options)
+    set(oneValue   TARGET)
+    set(multiValue INCLUDES LINKS)
+    cmake_parse_arguments(ARG "${options}" "${oneValue}" "${multiValue}" ${ARGN} )
+    # object library
     add_library(${ARG_TARGET} OBJECT)
     # dependencies
     target_include_directories(${ARG_TARGET} 
@@ -37,8 +55,13 @@ function(create_interface)
     foreach(L ${ARG_LINKS})
         target_include_directories(${ARG_TARGET} 
             PUBLIC $<TARGET_PROPERTY:${L},INTERFACE_INCLUDE_DIRECTORIES>)
-        target_sources(${ARG_TARGET} 
-            PUBLIC $<TARGET_OBJECTS:${L}>)
+        get_target_property(type ${L} TYPE)
+        message("----------------->${type}")
+        if (type STREQUAL "EXECUTABLE")
+            target_sources(${ARG_TARGET} 
+                PUBLIC $<TARGET_OBJECTS:${L}>)
+        endif ()
+        
     endforeach()
 endfunction()
 # -------------------------------------------------------------------------------------------------
@@ -49,31 +72,25 @@ function(create_library)
     set(oneValue   TARGET)
     set(multiValue SOURCES PUBLIC_INCLUDES PRIVATE_INCLUDES PUBLIC_LINKS PRIVATE_LINKS)
     cmake_parse_arguments(ARG "${options}" "${oneValue}" "${multiValue}" ${ARGN} )
-    # object library
-    add_library(${ARG_TARGET}
-        ${ARG_SOURCES}
-    )
+    # library
+    add_library(${ARG_TARGET} ${ARG_SOURCES})
     # dependencies
     target_include_directories(${ARG_TARGET} 
-    PUBLIC
-        ${ARG_PUBLIC_INCLUDES}
-    PRIVATE
-        ${ARG_PRIVATE_INCLUDES}
-    )
+        PUBLIC  ${ARG_PUBLIC_INCLUDES}
+        PRIVATE ${ARG_PRIVATE_INCLUDES})
     foreach(L ${ARG_PUBLIC_LINKS})
         target_include_directories(${ARG_TARGET} 
-        PUBLIC 
-            $<TARGET_PROPERTY:${L},INTERFACE_INCLUDE_DIRECTORIES>
-        )
+            PUBLIC $<TARGET_PROPERTY:${L},INTERFACE_INCLUDE_DIRECTORIES>)
+        target_sources(${ARG_TARGET} 
+            PUBLIC $<TARGET_OBJECTS:${L}>)
     endforeach()
     foreach(L ${ARG_PRIVATE_LINKS})
         target_include_directories(${ARG_TARGET} 
-        PRIVATE 
-            $<TARGET_PROPERTY:${L},INTERFACE_INCLUDE_DIRECTORIES>
-        )
+            PRIVATE $<TARGET_PROPERTY:${L},INTERFACE_INCLUDE_DIRECTORIES>)
+        target_sources(${ARG_TARGET} 
+            PRIVATE $<TARGET_OBJECTS:${L}>)
     endforeach()
 endfunction()
-
 # -------------------------------------------------------------------------------------------------
 # create executable
 # -------------------------------------------------------------------------------------------------
