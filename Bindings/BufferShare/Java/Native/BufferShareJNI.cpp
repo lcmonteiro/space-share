a50054a59e65c58314d3f5c9350bdf5d7db12a9c
Monteiro <monteiro.lcm@gmail.com>
2019-09-10 00:04:24 +0100

    update: BufferShare JNI

--- Bindings/BufferShare/Java/Native/BufferShareJNI.cpp
+++ Bindings/BufferShare/Java/Native/BufferShareJNI.cpp
@@ -1,103 +1,142 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-#include <stdio.h>
 /**
+ * --------------------------------------------------------------------------------------------------------------------
+ * File:   BufferShareJNI.cpp
+ * Author: Luis Monteiro
+ *
+ * Created on June 3, 2015, 10:12 AM
+ * -------------------------------------------------------------------------------------------------------------------- 
+ **
+ * std
  */
+#include <cstdio>
 #include <map>
 /**
  * JNI interface
  */
 #include "BufferShareJNI.h"
-/*---------------------------------------------------------------------------------------------------------------------*
- * definitions
- *---------------------------------------------------------------------------------------------------------------------*/
+/**
+ * --------------------------------------------------------------------------------------------------------------------
+ * Definitions
+ * --------------------------------------------------------------------------------------------------------------------
+ **/
 typedef enum {
-        ENCODER, DECODER
+    ENCODER, DECODER
 } Type;
-/*---------------------------------------------------------------------------------------------------------------------*
+/**
+ * --------------------------------------------------------------------------------------------------------------------
  * container
- *---------------------------------------------------------------------------------------------------------------------*/
+ * --------------------------------------------------------------------------------------------------------------------
+ **/
 static std::map<uint32_t, OBufferShare> __encoder;
 static std::map<uint32_t, IBufferShare> __decoder;
-/*---------------------------------------------------------------------------------------------------------------------*
+/**
+ * --------------------------------------------------------------------------------------------------------------------
  * std C functions
- *---------------------------------------------------------------------------------------------------------------------*/
+ * --------------------------------------------------------------------------------------------------------------------
+ **/
 extern "C" {
-        /*-------------------------------------------------------------------------------------------------------------*
-         * converters
-         *-------------------------------------------------------------------------------------------------------------*/
-        inline Frame toFrame(JNIEnv*& env, jbyteArray& data);
-        /**/
-        inline jbyteArray toByteArray(JNIEnv*& env, const Frame& data);
-        /*-------------------------------------------------------------------------------------------------------------*
-         * interfaces
-         *-------------------------------------------------------------------------------------------------------------*/
-        JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Open(
-                JNIEnv * env, jobject obj, jint coder, jint id
-        ) {
-                switch (coder) {
-                        case ENCODER: return __encoder.emplace(
-                                std::piecewise_construct,std::forward_as_tuple(id), std::forward_as_tuple()).second;
-                        case DECODER: return __decoder.emplace(
-                                std::piecewise_construct,std::forward_as_tuple(id), std::forward_as_tuple()).second;
-                }
-                return false;
+    /**
+     * --------------------------------------------------------------------------------------------
+     * Converters
+     * --------------------------------------------------------------------------------------------
+     **/
+    inline Frame toFrame(JNIEnv*& env, jbyteArray& data);
+
+    inline jbyteArray toByteArray(JNIEnv*& env, const Frame& data);
+    /**
+     * --------------------------------------------------------------------------------------------
+     * Interfaces
+     * --------------------------------------------------------------------------------------------
+     * open
+     * ------------------------------------------------------------------------
+     **/
+    JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Open(
+        JNIEnv * env, jobject obj, jint coder, jint id
+    ) {
+        switch (coder) {
+            case ENCODER: return __encoder.emplace(
+                std::piecewise_construct,std::forward_as_tuple(id), std::forward_as_tuple()).second;
+            case DECODER: return __decoder.emplace(
+                std::piecewise_construct,std::forward_as_tuple(id), std::forward_as_tuple()).second;
         }
-        /**
-         */
-        JNIEXPORT jint JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Encode(
-                JNIEnv * env, jobject obj, jbyteArray data, jint size, jint id
-        ) {
-                return __encoder[id].set(toFrame(env, data), static_cast<size_t>(size));
+        return false;
+    }
+    /**
+     * ------------------------------------------------------------------------
+     * Encode
+     * ------------------------------------------------------------------------
+     */
+    JNIEXPORT jint JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Encode(
+        JNIEnv * env, jobject obj, jbyteArray data, jint size, jint id
+    ) {
+        return __encoder[id].set(toFrame(env, data), static_cast<size_t>(size));
+    }
+    /**
+     * ------------------------------------------------------------------------
+     * Get Frame
+     * ------------------------------------------------------------------------
+     */
+    JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_GetFrame(
+        JNIEnv * env, jobject obj, jint id
+    ) {
+        return toByteArray(env, __encoder[id].get());
+    }
+    /**
+     * ------------------------------------------------------------------------
+     * Set Frame
+     * ------------------------------------------------------------------------
+     */
+    JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_SetFrame(
+        JNIEnv * env, jobject obj, jbyteArray data, jint id
+    ) {
+        return __decoder[id].set(toFrame(env, data));
+    }
+    /**
+     * ------------------------------------------------------------------------
+     * Decode
+     * ------------------------------------------------------------------------
+     */
+    JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Decode(
+        JNIEnv * env, jobject obj, jint id
+    ) {
+        return toByteArray(env, __decoder[id].get());
+    }
+    /**
+     * ------------------------------------------------------------------------
+     * Close
+     * ------------------------------------------------------------------------
+     */
+    JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Close(
+        JNIEnv * env, jobject obj, jint coder, jint id
+    ) {
+        switch (coder) {
+            case ENCODER: __encoder.erase(id); return true;
+            case DECODER: __decoder.erase(id); return true;
         }
-        /**
-         */
-        JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_GetFrame(
-                JNIEnv * env, jobject obj, jint id
-        ) {
-                return toByteArray(env, __encoder[id].get());
-        }
-        /**
-         */
-        JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_SetFrame(
-                JNIEnv * env, jobject obj, jbyteArray data, jint id
-        ) {
-                return __decoder[id].set(toFrame(env, data));
-        }
-        /**
-         */
-        JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Decode(
-                JNIEnv * env, jobject obj, jint id
-        ) {
-                return toByteArray(env, __decoder[id].get());
-        }
-        /**
-         */
-        JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Close(
-                JNIEnv * env, jobject obj, jint coder, jint id
-        ) {
-                switch (coder) {
-                        case ENCODER: __encoder.erase(id); return true;
-                        case DECODER: __decoder.erase(id); return true;
-                }
-                return false;
-        }
-        /*-------------------------------------------------------------------------------------------------------------*
-         * converters
-         *-------------------------------------------------------------------------------------------------------------*/
-        Frame toFrame(JNIEnv*& env, jbyteArray& array){
-                jsize len = env->GetArrayLength(array);
-                jbyte*body = env->GetByteArrayElements(array, 0);
-                Frame out(reinterpret_cast<uint8_t*>(body), reinterpret_cast<uint8_t*>(body) + static_cast<size_t>(len)); 
-                env->ReleaseByteArrayElements(array, body, 0);
-                return out;
-        }
-        jbyteArray toByteArray(JNIEnv*& env, const Frame& buf){
-                jbyteArray ret = env->NewByteArray(buf.size());
-                env->SetByteArrayRegion (ret, 0, buf.size(), reinterpret_cast<const jbyte*>(buf.data()));
-                return ret;
-        }
+        return false;
+    }
+    /**
+     * --------------------------------------------------------------------------------------------
+     * converters
+     * --------------------------------------------------------------------------------------------
+     */
+    Frame toFrame(JNIEnv*& env, jbyteArray& array){
+        jsize len = env->GetArrayLength(array);
+        jbyte*body = env->GetByteArrayElements(array, 0);
+        Frame out(
+            reinterpret_cast<uint8_t*>(body), 
+            reinterpret_cast<uint8_t*>(body) + static_cast<size_t>(len)); 
+        env->ReleaseByteArrayElements(array, body, 0);
+        return out;
+    }
+    jbyteArray toByteArray(JNIEnv*& env, const Frame& buf){
+        jbyteArray ret = env->NewByteArray(buf.size());
+        env->SetByteArrayRegion (ret, 0, buf.size(), reinterpret_cast<const jbyte*>(buf.data()));
+        return ret;
+    }
 }
+/**
+ * --------------------------------------------------------------------------------------------------------------------
+ * End
+ * --------------------------------------------------------------------------------------------------------------------
+ */
--- Bindings/BufferShare/Java/Native/BufferShareJNI.h
+++ Bindings/BufferShare/Java/Native/BufferShareJNI.h
@@ -1,71 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-#include <jni.h>
-/* 
- * File:   ShareJNI.h
+/**
+ * ------------------------------------------------------------------------------------------------ 
+ * File:   BufferShareJNI.h
  * Author: Luis Monteiro
  *
- * Created on October 30, 2016, 4:22 PM
- */
-#include "SBufferShare.h"
-/**
- * 
+ * Created on November 26, 2016, 12:37 PM
+ * ------------------------------------------------------------------------------------------------
  */
 #ifndef BUFFERSHAREJNI_H
 #define BUFFERSHAREJNI_H
-
+/**
+ * external
+ */
+#include <jni.h>
+/** 
+ * internal
+ */
+#include "SBufferShare.h"
+/**
+ * ------------------------------------------------------------------------------------------------
+ * Interfaces
+ * ------------------------------------------------------------------------------------------------
+ */
 extern "C" {
-        /*
-         * Class:     BufferSharee_BufferShare
-         * Method:    Open
-         * Signature: (II)Z
-         */
-        JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Open(
-                JNIEnv *, jobject, jint, jint
-        );
-        /*
-         * Class:     BufferShare_BufferShare
-         * Method:    Encode
-         * Signature: ([BII)I
-         */
-        JNIEXPORT jint JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Encode(
-                JNIEnv *, jobject, jbyteArray, jint, jint
-        );
-        /*
-         * Class:     BufferShare_BufferShare
-         * Method:    GetFrame
-         * Signature: (I)[B
-         */
-        JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_GetFrame(
-                JNIEnv *, jobject, jint
-        );
-        /*
-         * Class:     BufferShare_BufferShare
-         * Method:    SetFrame
-         * Signature: ([BI)Z
-         */
-        JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_SetFrame(
-                JNIEnv *, jobject, jbyteArray, jint
-        );
-        /*
-         * Class:     BufferShare_BufferShare
-         * Method:    Decode
-         * Signature: (I)[B
-         */
-        JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Decode(
-                JNIEnv *, jobject, jint
-        );
-        /*
-         * Class:     BufferShare_BufferShare
-         * Method:    Close
-         * Signature: (II)Z
-         */
-        JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Close(
-                JNIEnv *, jobject, jint, jint
-        );
+    /*
+    * Class:     BufferSharee_BufferShare
+    * Method:    Open
+    * Signature: (II)Z
+    */
+    JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Open(
+        JNIEnv *, jobject, jint, jint
+    );
+    /*
+    * Class:     BufferShare_BufferShare
+    * Method:    Encode
+    * Signature: ([BII)I
+    */
+    JNIEXPORT jint JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Encode(
+        JNIEnv *, jobject, jbyteArray, jint, jint
+    );
+    /*
+    * Class:     BufferShare_BufferShare
+    * Method:    GetFrame
+    * Signature: (I)[B
+    */
+    JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_GetFrame(
+        JNIEnv *, jobject, jint
+    );
+    /*
+    * Class:     BufferShare_BufferShare
+    * Method:    SetFrame
+    * Signature: ([BI)Z
+    */
+    JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_SetFrame(
+        JNIEnv *, jobject, jbyteArray, jint
+    );
+    /*
+    * Class:     BufferShare_BufferShare
+    * Method:    Decode
+    * Signature: (I)[B
+    */
+    JNIEXPORT jbyteArray JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Decode(
+        JNIEnv *, jobject, jint
+    );
+    /*
+    * Class:     BufferShare_BufferShare
+    * Method:    Close
+    * Signature: (II)Z
+    */
+    JNIEXPORT jboolean JNICALL Java_com_share_cwrapper_buffershare_BufferShare_Close(
+        JNIEnv *, jobject, jint, jint
+    );
 }
+/**
+ * ------------------------------------------------------------------------------------------------
+ * End
+ * ------------------------------------------------------------------------------------------------
+ */
 #endif /* BUFFERSHAREJNI_H */
 
